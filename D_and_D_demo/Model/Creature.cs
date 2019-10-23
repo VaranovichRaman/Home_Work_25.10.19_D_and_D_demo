4cc88db3fa1df4f823c57dc05bf00107a1144498
Raven Raven <raven_180691@mail.ru>
2019-10-23 20:26:29 +0300

    Add project files.

---
+++ D_and_D_demo/Model/Creature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_and_D_demo.Model
+{
+    class Creature
+    {
+        public string CreatureName { get; set; }
+        public Weapon CreatureWeapon { get; set; }
+        public ArmorClass CreatureArmor { get; set; }
+        public AttackMod CreatureAttackMod { get; set; }
+        public int CreatureHitPoints { get; set; }
+        public int CreatureLevel { get; set; }
+    }
+}
